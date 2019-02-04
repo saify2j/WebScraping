8f9b120855610565935b68750d6dafdf9a0dfcde
saify2j <bsse0924@iit.du.ac.bd>
2019-02-04 11:24:59 +0600

    updated 31st jan

--- ryans.py
+++ ryans.py
@@ -3,22 +3,24 @@ import requests
 
 
 def get_last_page(url, component):
-    main_page = requests.get(url + component)
+    main_page = requests.get(url + component+".html")
     soup = BeautifulSoup(main_page.content, 'html.parser')
-    #print(soup.prettify())
-    page_list=[]
-    for a in soup.select('.pager'):
-       # page_list.append(a.get_text())
-        print(a.get_text())
-#    page_list = soup.findAll(attrs={"class": "pager"})
+    # print(soup.prettify())
+    number_of_pages = []
+    for a in soup.select('.pages li a'):
+        number_of_pages.append(a.get_text())
 
-    # print(page_list)
+    last_page = int(number_of_pages[-2])
+    return last_page
 
+
+def load_data(url,component,last_page):
+    page = requests.get(url+component+"html?p="")
 
 def main():
     url = "https://ryanscomputers.com/components/"
     component = "processor"
-    get_last_page(url, component)
+    last_page = get_last_page(url, component)
 
 
 if __name__ == '__main__':
